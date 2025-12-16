bae89d1052920d4bb9294a11615a99cf779d7c81
Mundher Noaman Yousif <117886624+MundherYousif@users.noreply.github.com>
2025-12-16 22:54:41 +0000

    Add files via upload

---
+++ cleanup.ps1
@@ -0,0 +1,8 @@
+﻿$FilePath = "C:\Scripts\Onboardv4.xlsx"
+
+if (Test-Path $FilePath) {
+    Remove-Item $FilePath -Force
+    Write-Output "Deleted old file: $FilePath"
+} else {
+    Write-Output "No old file found to delete."
+}
